88261de61223163867ec4302e980fecba44430ce
Jon Gentry <jonmarkgentry@gmail.com>
2022-09-16 23:22:40 -0700

    Add home view calorie total cards

---
+++ Intake/Views/CalorieTotalView.swift
@@ -0,0 +1,43 @@
+//
+//  CalorieTotalView.swift
+//  Intake
+//
+//  Created by Jonathan Gentry on 9/16/22.
+//
+
+import SwiftUI
+
+struct CalorieTotalView: View {
+    
+    var title: String
+    var amount: String
+    
+    var body: some View {
+        VStack(spacing: 18) {
+            Spacer()
+            VStack {
+                Text(title)
+                    .font(.title)
+                    .foregroundColor(.secondary)
+                Spacer()
+                    .frame(height: 20)
+                Text(amount)
+                    .font(.largeTitle)
+                Text("kCal")
+                    .foregroundColor(.secondary)
+                Spacer()
+                    .frame(height: 50)
+                
+                }
+                .frame(width: 300, height: 200)
+                .background(.ultraThinMaterial)
+                .cornerRadius(30)
+        }
+    }
+}
+
+//struct CalorieTotalView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        CalorieTotalView()
+//    }
+//}
