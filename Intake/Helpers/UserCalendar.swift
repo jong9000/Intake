77927e05f98baff3b95cd6d1d014797203e198c1
Jon Gentry <jonmarkgentry@gmail.com>
2022-09-28 22:14:06 -0700

    Correct total for yesterday, day before that, and day before that

--- Intake/Helpers/UserCalendar.swift
+++ Intake/Helpers/UserCalendar.swift
@@ -14,4 +14,16 @@ struct UserCalendar {
     var startOfDay: Date {
         userCalendar.startOfDay(for: currentDate)
     }
+    
+    var startOfYesterday: Date {
+        userCalendar.startOfDay(for: userCalendar.date(byAdding: .day, value: -1, to: currentDate)!)
+    }
+    
+    var startOfDayBeforeYesterday: Date {
+        userCalendar.startOfDay(for: userCalendar.date(byAdding: .day, value: -2, to: currentDate)!)
+    }
+    
+    var startOfDayBeforeDayBeforeYesterday: Date {
+        userCalendar.startOfDay(for: userCalendar.date(byAdding: .day, value: -3, to: currentDate)!)
+    }
 }
