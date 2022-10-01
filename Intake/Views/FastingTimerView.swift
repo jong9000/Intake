d4c4b5613b612ce155f5b6e9c3777378d902d336
Jon Gentry <jonmarkgentry@gmail.com>
2022-09-30 20:33:34 -0700

    Add extensions for single item fetch request and date formatting

---
+++ Intake/Views/FastingTimerView.swift
@@ -0,0 +1,37 @@
+//
+//  FastingTimerView.swift
+//  Intake
+//
+//  Created by Jonathan Gentry on 9/30/22.
+//
+
+import SwiftUI
+
+struct FastingTimerView: View {
+        
+    @FetchRequest(fetchRequest: Item.fetchLastItem) private var lastItem: FetchedResults<Item>
+    
+    var body: some View {
+        VStack(spacing: 18) {
+            Spacer()
+            VStack {
+                Text("Fasting Since")
+                    .font(.title)
+                    .foregroundColor(.secondary)
+                Text(lastItem[0].timestamp ?? Date(), formatter: Date.itemFormatter)
+                Spacer()
+                    .frame(height: 20)
+                Text("6 hours")
+                    .font(.largeTitle)
+                Text("12 minutes")
+                    .foregroundColor(.secondary)
+                Spacer()
+                    .frame(height: 50)
+                
+                }
+                .frame(width: 300, height: 200)
+                .background(.ultraThinMaterial)
+                .cornerRadius(30)
+        }
+    }
+}
